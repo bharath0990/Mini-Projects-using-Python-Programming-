d97108290450717cda0076183f736c197c5f50d0
BHARATH KUMAR REDDY <157892607+bharath0990@users.noreply.github.com>
2024-08-07 00:22:16 +0530

    Add files via upload

---
+++ miniproject-02.py
@@ -0,0 +1,15 @@
+import random
+val = random.randint(1,100)
+while True:
+    num = int(input("Enter the number or press Q to exit = ")) 
+    if(num == "Q"):
+        break
+    if(num == val):
+        print("sucess : Correct Guess!!")
+        break
+    elif(num > val):
+        print("your num was bigger. Take the smaller guess....")
+    else:
+        print("your num was smaller. Take the bigger guess....")
+
+print("-----GAME OVER----")
